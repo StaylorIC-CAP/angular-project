854cd3ec4d564e59fa5a3c62124e7095af36affb
StaylorIC-CAP <steve@ic-cap.com>
2019-06-20 15:42:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vejexu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vejexu)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Lane Taylor',
+    price: 799,
+    description: 'Changed my mind.  Fire him!'
+  },
+  {
+    name: 'Larry Williams',
+    price: 699,
+    description: 'Learning to code'
+  },
+  {
+    name: 'Steve Taylor',
+    price: 299,
+    description: 'Fire Him!'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
